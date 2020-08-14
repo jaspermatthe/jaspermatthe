85f654261dc8ced30199670af36387f6c8446913
Jasper Matthe <matthejasper@gmail.com>
2020-08-13 22:14:43 -0700

    l

--- Desktop/lastlast/src/pages/index.js
+++ Desktop/lastlast/src/pages/index.js
@@ -12,6 +12,19 @@ const Index = () => {
         <p>My name is Jasper.
           Welcome to my website!
         </p>
+
+        <h2>A little about me...</h2>
+        <p>I'm a seventeen-year-old student at Semiahmoo Secondary School in South Surrey, British Columbia...<br></br>
+        I'm enrolled in the International Baccalaureate Program..<br></br>
+        I play soccer and I love it...<br></br>
+        I love to travel...<br></br>
+        I want to be an architect...<br></br>
+        I hope to share all my interests with the world through this website...<br></br>
+        Have a browse to learn a bit more about me!
+        
+
+
+        </p>
       </div>
     </Layout>
   )
